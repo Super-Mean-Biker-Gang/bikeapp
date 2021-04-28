5a7feded465c5f6c6de5291586efec1a03397c12
Travis <tmoret058@gmail.com>
2021-04-28 02:41:38 -0500

    Added ability to ask for location permission and center on user location with floating action button

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,18 +1,87 @@
 import 'package:flutter/material.dart';
 import 'package:bikeapp/widgets/map_end_drawer.dart';
 import 'package:bikeapp/widgets/map_drawer.dart';
-import 'package:bikeapp/widgets/map_fab.dart';
 import 'package:bikeapp/widgets/map_view.dart';
+import 'package:geolocator/geolocator.dart';
+import 'dart:async';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MapScreen extends StatelessWidget {
+class MapScreen extends StatefulWidget {
   static const routeName = 'map_screen';
+  @override
+  MapScreenState createState() => MapScreenState();
+}
+
+class MapScreenState extends State<MapScreen> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static CameraPosition _userPosition;
+
+  void initState() {
+    super.initState();
+    determinePosition();
+  }
+
+  Future<void> goToPosition(CameraPosition position) async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(position));
+  }
+
+  void setUserPosition(Position position) {
+    if (position.latitude != null && position.longitude != null) {
+      _userPosition = CameraPosition(
+        target: LatLng(position.latitude, position.longitude),
+        zoom: 14.4746,
+      );
+    }
+  }
+
+  determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+
+    if (!serviceEnabled) {
+      print('Location services are disabled.');
+    }
+
+    // Check permissions
+    permission = await Geolocator.checkPermission();
+
+    // If permission granted, set user location and move map to it
+    if ((permission == LocationPermission.always ||
+            permission == LocationPermission.whileInUse) &&
+        serviceEnabled) {
+      Position position = await Geolocator.getCurrentPosition();
+      setUserPosition(position);
+      goToPosition(_userPosition);
+    } else if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+
+      if (permission == LocationPermission.denied) {
+        print('Location permissions are denied');
+      }
+    } else if (permission == LocationPermission.deniedForever) {
+      print(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: MapView(),
-        endDrawer: MapEndDrawer(),
-        drawer: MapDrawer(),
-        floatingActionButton: MapFab());
+      body: MapView(controller: _controller),
+      endDrawer: MapEndDrawer(),
+      drawer: MapDrawer(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          determinePosition();
+        },
+        tooltip: 'Center map on Location',
+        child: Icon(Icons.my_location),
+      ),
+    );
   }
 }
--- lib/widgets/map_view.dart
+++ lib/widgets/map_view.dart
@@ -1,85 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'dart:async';
 import 'package:bikeapp/widgets/map_search_bar.dart';
-import 'package:geolocator/geolocator.dart';
+import 'dart:async';
 
-class MapView extends StatefulWidget {
-  @override
-  _MapViewState createState() => _MapViewState();
-}
-
-class _MapViewState extends State<MapView> {
-  Completer<GoogleMapController> _controller = Completer();
-
-  static CameraPosition _userPosition;
-
-  @override
-  void initState() {
-    super.initState();
-    _determinePosition();
-  }
+class MapView extends StatelessWidget {
+  final Completer<GoogleMapController> _controller;
+  // final CameraPosition userPosition;
+  MapView({Completer<GoogleMapController> controller})
+      : _controller = controller;
 
   // Initial Position in Downtown Denver, CO
   static final CameraPosition _defaultPosition = CameraPosition(
     target: LatLng(39.7527, -105.0017),
     zoom: 14.4746,
   );
-
-  // Send the camera to a new camera position
-  Future<void> _goToPosition(CameraPosition position) async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(position));
-  }
-
-  // Set the initial value of the user's location after retrieving it
-  void setUserPosition(Position position) {
-    if (position.latitude != null && position.longitude != null) {
-      _userPosition = CameraPosition(
-        target: LatLng(position.latitude, position.longitude),
-        zoom: 14.4746,
-      );
-    }
-  }
-
-  // Request permission and determine the current position of the device
-  void _determinePosition() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-
-    // Test if location services are enabled.
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-
-    if (!serviceEnabled) {
-      print('Location services are disabled.');
-    }
-
-    // Check permissions
-    permission = await Geolocator.checkPermission();
-
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-
-      if (permission == LocationPermission.denied) {
-        print('Location permissions are denied');
-      }
-    }
-
-    if (permission == LocationPermission.deniedForever) {
-      print(
-          'Location permissions are permanently denied, we cannot request permissions.');
-    }
-
-    // If permission granted, set user location and move map to it
-    if ((permission == LocationPermission.always ||
-            permission == LocationPermission.whileInUse) &&
-        serviceEnabled) {
-      Position position = await Geolocator.getCurrentPosition();
-      setUserPosition(position);
-      _goToPosition(_userPosition);
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
